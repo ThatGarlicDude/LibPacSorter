1f7494624e672100e7afdb5ab1fcc75d371aa0b3
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-05-25 20:19:34 -0400

    Removed the printf call.

--- src/romSorter.c
+++ src/romSorter.c
@@ -9,7 +9,6 @@ void sortROMs(RomSet* romSet) {
 	size_t size = romSet->size;
 	for (size_t index = 0; index < size; index++) {
 		int lowestROM = findLowestROM(romSet, index);
-		printf("Lowest ROM points to: %p.\n", &romSet->roms[lowestROM]);
 		swapROMs(&romSet->roms[index], &romSet->roms[lowestROM]);
 	}
 	return;
